029e74230b76c6f0342ca9d3b6ca093a2df4966d
LAPTOP-DHHLQT9N\wert7 <wert723jim@gmail.com>
2023-08-30 11:19:51 +0800

    feat: add uses page

---
+++ pages/uses.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="text-3xl font-bold my-8">Uses</h1>
+  <p class="text-lg">
+    This page is to show what I use to build this Website. If you have any questions or suggestions, you can reach out to me on <a href="https://www.twitter.com">Twitter</a>. This website was cloned from Dan Vega's <a href="https://www.youtube.com/watch?v=b6b2yZZNG6Y&ab_channel=NetNinja">net-ninja-portfolio project</a> .
+  </p>
+
+  <h2 class="text-2xl font-bold mt-8 mb-2">This Website</h2>
+  <ul class="list-disc">
+    <li class="ml-8 py-1"><a href="https://vuejs.org/">Vue 3</a></li>
+    <li class="ml-8 py-1"><a href="https://nuxt.com/">Nuxt 3</a></li>
+    <li class="ml-8 py-1"><a href="https://tailwindcss.com/">Tailwind CSS</a></li>
+    <li class="ml-8 py-1">Hosted on <a href="https://netlify.com/">Netlify</a></li>
+  </ul>
+
+  <h2 class="text-2xl font-bold mt-8 mb-2">Editor</h2>
+  <ul class="list-disc">
+    <li class="ml-8 py-1"><a href="https://code.visualstudio.com/">Visual Studio</a></li>
+  </ul>
+</template>
